bd3dc6347a2c92464c2e271d24e55d8151901e63
Brian Wynn <brian.wynn83@gmail.com>
2015-07-08 08:49:07 -0700

    CLeaned up and pushed to github so that I can start working on my work computer as well.

--- ch1_spa/script.js
+++ ch1_spa/script.js
@@ -1,4 +1,11 @@
 // js lint settings
+/*jslint
+  browser : true, continue : true,
+  devel : true, indent : 2, maxerr : 50,
+  newcap : true, nomen : true, plusplus : true,
+  regexp : true, sloppy : true, vars : true,
+  white : true
+*/
 
 // module /spa/
 // provides chat slider capability
