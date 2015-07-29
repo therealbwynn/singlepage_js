da7faf8531496a767c8ce1779a23673d0d3c52e6
Brian Wynn <b_wynn@apple.com>
2015-07-29 10:17:28 -0700

    Finished off the spa.util.js module and added script and styles to the spa.html page. Also called and initialized the spa.chat.js functionality within the shell.

--- ch4/spa/js/spa.util.js
+++ ch4/spa/js/spa.util.js
@@ -42,4 +42,29 @@ spa.util = (function() {
   // * config_map - map to apply settings to
   // Returns : true
   // Throws : Exception if input key not allowed.
+  //
+  setConfigMap = function ( arg_map ) {
+    var
+      input_map         = arg_map.input_map,
+      settable_map      = arg_map.settable_map,
+      config_map        = arg_map.config_map,
+      key_name, error;
+
+    for ( key_name in input_map ) {
+      if ( settable_map.hasOwnProperty( key_name ) ) {
+        config_map[key_name] = input_map[key_name];
+      }
+      else {
+        error = makeError( 'Bad Input',
+          'Setting config key |' + key_name + '| is not supported');
+          throw error;
+      }
+    }
+  };
+  // End Public Method /setConfigMap/
+
+  return {
+    makeError: makeError,
+    setConfigMap: setConfigMap
+  };
 }());
